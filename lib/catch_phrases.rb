9893230808cd2404f3416fe75bbe31ffd660a2ba
Mark De La Rosa <mark.p.dlr@gmail.com>
2020-04-01 21:22:37 +0000

    Done.

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,25 @@
+
+def all_phrases
+  mario
+  toadstool
+  link
+end
+
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+ # status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  phrase = "It's-a me, Mario!"
   puts phrase
+  phrase
 end
 
 def toadstool
-  puts status
+  phrase = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  puts phrase
+  phrase
+end
+
+def link
+  phrase = "It's Dangerous To Go Alone! Take This."
+  puts phrase
+  phrase
 end
